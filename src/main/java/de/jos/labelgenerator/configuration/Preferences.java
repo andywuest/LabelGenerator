98103d77e6caf0bdc5f826e38dcec82347337253
andywuest <andreas.wuest.freelancer@googlemail.com>
2011-03-04 21:49:56 +0100

    preferences for gmail provider

---
+++ src/main/java/de/jos/labelgenerator/configuration/Preferences.java
@@ -0,0 +1,58 @@
+package de.jos.labelgenerator.configuration;
+
+public class Preferences {
+
+	private String gmailEmail = null;
+
+	private String gmailGroup = null;
+
+	private transient String gmailPassword = null;
+
+	private boolean checkboxFilesystemProvider = true;
+
+	private boolean checkboxGMailProvider = false;
+
+	public Preferences() {
+	}
+
+	public String getGmailEmail() {
+		return gmailEmail;
+	}
+
+	public void setGmailEmail(String gmailEmail) {
+		this.gmailEmail = gmailEmail;
+	}
+
+	public String getGmailGroup() {
+		return gmailGroup;
+	}
+
+	public void setGmailGroup(String gmailGroup) {
+		this.gmailGroup = gmailGroup;
+	}
+
+	public String getGmailPassword() {
+		return gmailPassword;
+	}
+
+	public void setGmailPassword(String gmailPassword) {
+		this.gmailPassword = gmailPassword;
+	}
+
+	public boolean getCheckboxFilesystemProvider() {
+		return checkboxFilesystemProvider;
+	}
+
+	public void setCheckboxFilesystemProvider(boolean checkboxFilesystemProvider) {
+		this.checkboxFilesystemProvider = checkboxFilesystemProvider;
+	}
+
+	public boolean getCheckboxGMailProvider() {
+		return checkboxGMailProvider;
+	}
+
+	public void setCheckboxGMailProvider(boolean checkboxGMailProvider) {
+		this.checkboxGMailProvider = checkboxGMailProvider;
+	}
+
+}
