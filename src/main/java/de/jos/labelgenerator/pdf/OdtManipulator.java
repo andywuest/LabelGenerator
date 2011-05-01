5f344e01080511a6168aa9a547da1547313eebc9
andywuest <andreas.wuest.freelancer@googlemail.com>
2011-05-01 16:34:49 +0200

    initial import

---
+++ src/main/java/de/jos/labelgenerator/pdf/OdtManipulator.java
@@ -0,0 +1,13 @@
+package de.jos.labelgenerator.pdf;
+
+public class OdtManipulator {
+
+	
+	// TODO read template odt file and extract to tmp directory
+	// replace content xml and recreate odt file.
+	// convert odt to pdf 
+	// print
+	
+	
+	
+}
